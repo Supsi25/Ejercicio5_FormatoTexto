b536dd4fd580e34691b4ff25a9b8683c8ce94d96
susana <susana.casas@iesdoctorbalmis.com>
2020-10-26 16:01:21 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio5_FormatoTexto/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Ejercicio5_FormatoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            colorAzul.IsChecked = true;
+        }
+
+        private void TextoNegrita_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textoNegrita.IsChecked == true)
+                textoDefinitivo.FontWeight = FontWeights.Bold;
+            else
+                textoDefinitivo.FontWeight = FontWeights.Normal;
+        }
+
+        private void TextoCursiva_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textoCursiva.IsChecked == true)
+                textoDefinitivo.FontStyle = FontStyles.Italic;
+            else
+                textoDefinitivo.FontStyle = FontStyles.Normal;
+        }
+
+        private void ColorAzul_Checked(object sender, RoutedEventArgs e)
+        {
+            textoDefinitivo.Foreground = Brushes.Blue;
+        }
+
+        private void ColorRojo_Checked(object sender, RoutedEventArgs e)
+        {
+            textoDefinitivo.Foreground = Brushes.Red;
+        }
+
+        private void ColorVerde_Checked(object sender, RoutedEventArgs e)
+        {
+            textoDefinitivo.Foreground = Brushes.Green;
+        }
+
+        private void CuadroTexto_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textoDefinitivo.Text = cuadroTexto.Text;
+        }
+    }
+}
